3d4b5e1d50bce89523d91d91615c72d0317f3480
Asztrik <asztrikpetrovai@gmail.com>
2023-02-22 11:57:27 +0100

    2metodus metodusok átnevezése

--- src/metodus_gyakorlas/Metodus_gyakorlas.java
+++ src/metodus_gyakorlas/Metodus_gyakorlas.java
@@ -3,15 +3,16 @@ package metodus_gyakorlas;
 public class Metodus_gyakorlas {
 
     public static void main(String[] args) {
-        
+
     }
- private static void also10(){   
+    
+ private static void elso10szamosszege(){   
    int osszeg = 0;
    for (int i = 0; i < 10; i++) {
     osszeg +=1;
  }
 }
-    private static int szam10() {
+    private static int osszead() {
         int a = 0;
         int b = 0;
         int szam = a+b;
@@ -20,7 +21,7 @@ public class Metodus_gyakorlas {
     }
 
     private static void kiir(int a, int b, int szam) {
-        System.out.printf("%d + %d = %d", a, b, szam);
+        System.out.printf("%d + %d = %d\n", a, b, szam);
     }
 
  
