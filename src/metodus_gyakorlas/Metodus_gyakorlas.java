abfd37e884c3de849b46fb89b9e64db3c0f88b29
Asztrik <asztrikpetrovai@gmail.com>
2023-02-22 12:23:07 +0100

    kiir javítása

--- src/metodus_gyakorlas/Metodus_gyakorlas.java
+++ src/metodus_gyakorlas/Metodus_gyakorlas.java
@@ -3,28 +3,27 @@ package metodus_gyakorlas;
 public class Metodus_gyakorlas {
 
     public static void main(String[] args) {
-
+        String szoveg = "";
+        kiir(szoveg);
     }
-    
- private static int elso10szamosszege(){   
-   int osszeg = 0;
-   for (int i = 0; i < 10; i++) {
-    osszeg +=1;
- }
-   return osszeg;
-}
+
+    private static int elso10szamosszege() {
+        int osszeg = 0;
+        for (int i = 0; i < 10; i++) {
+            osszeg += 1;
+        }
+        return osszeg;
+    }
+
     private static int osszead(int a, int b) {
-        return a+b;
+        return a + b;
     }
 
     private static void kiir(String szoveg) {
-        szoveg = Integer.toString( elso10szamosszege());
+        szoveg = Integer.toString(elso10szamosszege());
         System.out.println(szoveg);
-        szoveg = Integer.toString( osszead(1 ,2));
+        szoveg = Integer.toString(osszead(1, 2));
         System.out.println(szoveg);
     }
 
- 
- 
- 
 }
