decfa4a6196d70da90e56c072a258e2667b8c4ae
Asztrik <asztrikpetrovai@gmail.com>
2023-02-22 13:40:59 +0100

    random-nem random

--- src/metodus_gyakorlas/equalizer.java
+++ src/metodus_gyakorlas/equalizer.java
@@ -8,15 +8,17 @@ public class Equalizer {
     }
 
     private static void kockak() {
-            for (int i = 0; i < 5; i++) {
-                eq();
-        }
+            eq(0);
+            eq(0);
+            eq(0);
+            eq(0);
+            eq(0);
     }
 
-    private static void eq() {
+    private static void eq(int a) {
         int min = 3;
         int max = 7;
-
+        if (a == 0){
         int rand = (int) (Math.random() * (max - min + 1) + min);
         int index = 0;
         while (index < rand) {
@@ -24,5 +26,14 @@ public class Equalizer {
             index++;
         }
         System.out.println();
+    }
+        else{
+            int index = 0;
+            while (index < a){
+                System.out.printf("\u001B[45m" + " ");
+                index++;
+            }
+            System.out.println("");
+        }
     }
 }
